0c0f87978e5752de6862682442a07cd9bcae2d25
Akshay r <72512747+akshay0091@users.noreply.github.com>
2022-07-24 19:40:01 +0530

    Update settings.py

--- medical/settings.py
+++ medical/settings.py
@@ -116,13 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 #STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
