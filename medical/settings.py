5fec83ab5fcfbf1519fd2411cd03095c34b5993a
Akshay r <72512747+akshay0091@users.noreply.github.com>
2022-07-24 19:26:20 +0530

    Update settings.py

--- medical/settings.py
+++ medical/settings.py
@@ -125,3 +125,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
